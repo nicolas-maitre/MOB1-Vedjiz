8eb3e8774bce5eded715bf7637dfed4d4cd1426b
Diogo <diogof-6@hotmail.com>
2020-08-07 22:50:27 +0200

    add style on click to button

--- components/Login.js
+++ components/Login.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, View, TextInput, TouchableOpacity, Button, Alert } from 'react-native';
+import { Text, View, TextInput, TouchableOpacity, TouchableHighlight, Alert } from 'react-native';
 import Styles from '../src/styles/Register_Login';
 import Icon from 'react-native-vector-icons/Ionicons';
 export default class Login extends Component {
@@ -7,12 +7,13 @@ export default class Login extends Component {
         super()
         this.state = {
             showPassword: false,
-            tokken: null
+            tokken: null,
+            submitHover: false
         }
     }
 
     showPassword() {
-        this.setState({showPassword: !this.state.showPassword});
+        this.setState({ showPassword: !this.state.showPassword });
     }
 
     login(data) {
@@ -29,16 +30,22 @@ export default class Login extends Component {
                         placeholderTextColor="rgb(180, 180, 180)"
                         placeholder="re5t40-89sdf7-v96dc5"
                         value={this.state.tokken}
-                        onChangeText={value => this.setState({tokken: value})}
+                        onChangeText={value => this.setState({ tokken: value })}
                         secureTextEntry={!this.state.showPassword} />
-                    <TouchableOpacity 
+                    <TouchableOpacity
                         style={Styles.btnEye}
                         onPress={this.showPassword.bind(this)}
-                        >
-                        <Icon name={ this.state.showPassword ? "ios-eye-off-outline" : "ios-eye-outline" } size={26} color="white" />
+                    >
+                        <Icon name={this.state.showPassword ? "ios-eye-off-outline" : "ios-eye-outline"} size={26} color="white" />
                     </TouchableOpacity>
                 </View>
-                <Button title="Se connecter" onPress={() => this.login(this.state.tokken)}/>
+
+                <View style={this.state.submitHover ? Styles.submitHover : Styles.submit}
+                    onTouchStart={() => { this.setState({ submitHover: true }), this.login(this.state.tokken) }}
+                    onTouchEnd={() => this.setState({ submitHover: false })}
+                >
+                    <Text style={Styles.label} >Se connecter</Text>
+                </View>
             </View>
         )
     }
--- src/styles/Register_Login.js
+++ src/styles/Register_Login.js
@@ -6,19 +6,19 @@ const styles = StyleSheet.create({
         color: 'white',
     },
     inputGroups: {
-        flex: 1, 
+        flex: 1,
         flexDirection: "row",
         justifyContent: "space-between",
         alignItems: "center",
         marginBottom: 10,
-        paddingLeft:20,
-        paddingRight:20,
+        paddingLeft: 20,
+        paddingRight: 20,
         borderColor: 'transparent',
         borderWidth: 1,
         borderRadius: 100,
         backgroundColor: 'rgba(0, 0, 0, 0.45)',
         color: 'white',
-    },  
+    },
     textInput: {
         flex: 1,
         color: 'white',
@@ -31,6 +31,36 @@ const styles = StyleSheet.create({
     },
     btnEye: {
         paddingLeft: 10,
+    },
+    submit: {
+        borderRadius: 100,
+        width: '40%',
+        backgroundColor: 'rgba(35, 71, 205, 0.9)',
+        alignSelf: "center",
+        textAlign: "center",
+        alignItems: "center",
+        padding: 10,
+        paddingLeft: 20,
+        color: 'white',
+        shadowColor: 'black',
+        shadowRadius: 3,
+        shadowOffset: {
+            height: 5,
+            width: 5
+        },
+        shadowOpacity: 1,
+        elevation: 10,
+    },
+    submitHover: {
+        borderRadius: 100,
+        width: '40%',
+        backgroundColor: 'rgba(79, 104, 205, 0.9)',
+        alignSelf: "center",
+        textAlign: "center",
+        alignItems: "center",
+        padding: 10,
+        paddingLeft: 20,
+        color: 'white',        
     }
 });
 
