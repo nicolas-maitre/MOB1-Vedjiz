469b87d9e2aacae0d8f6dd6f288c050b4e023138
Diogo <diogof-6@hotmail.com>
2020-08-09 21:13:32 +0200

    create helpers

---
+++ components/Helpers.js
@@ -0,0 +1,9 @@
+
+export function isEmpty() {
+    var args = arguments;
+    for (let index = 0; index < arguments.length; index++) {
+        if (args[index] == null || args[index].replace(/\s/g, '') == '')
+            return true;
+    }
+    return false;
+}
