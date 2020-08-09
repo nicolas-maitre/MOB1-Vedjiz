990136e66c4ac4430c983a87c05a30440c60a116
Diogo <diogof-6@hotmail.com>
2020-08-09 21:15:10 +0200

    create button and events

--- components/Register.js
+++ components/Register.js
@@ -1,8 +1,23 @@
 import React, { Component } from 'react';
 import Styles from '../src/styles/Register_Login';
-import { Text, View, TextInput } from 'react-native';
+import { Text, View, TextInput, Alert } from 'react-native';
+import {isEmpty} from './Helpers';
 
 export default class Register extends Component {
+    constructor() {
+        super();
+        this.state = {
+            firstname: null,
+            lastname: null,
+            phonenumber: null,
+            submitHover: false
+        };
+    }
+
+    register(firstname, lastname, phonenumber){        
+        Alert.alert("Your Informations", `Firstname: ${firstname}\nLastname: ${lastname}\nPhonenumber: ${phonenumber}`);
+    }
+
     render() {
         return (
             <View style={Styles.container}>
@@ -11,14 +26,17 @@ export default class Register extends Component {
                     <TextInput
                         style={Styles.textInput}
                         placeholderTextColor="rgb(180, 180, 180)"
-                        placeholder="Diogo" />
+                        placeholder="Diogo"
+                        onChangeText={value => this.setState({ firstname: value })}/>
+
                 </View>
                 <View style={Styles.inputGroups}>
                     <Text style={Styles.label}>Nom:</Text>
                     <TextInput
                         style={Styles.textInput}
                         placeholderTextColor="rgb(180, 180, 180)"
-                        placeholder="Vieira" />
+                        placeholder="Vieira" 
+                        onChangeText={value => this.setState({ lastname: value })}/>
                 </View>
                 <View style={Styles.inputGroups}>
                     <Text style={Styles.label}>N°:</Text>
@@ -26,7 +44,20 @@ export default class Register extends Component {
                         style={Styles.textInput}
                         placeholder="Numéro de téléphone"
                         placeholderTextColor="rgb(180, 180, 180)"
-                        keyboardType="phone-pad" />
+                        keyboardType="phone-pad" 
+                        onChangeText={value => this.setState({ phonenumber: value })} />
+                </View>
+                
+                <View                 
+                pointerEvents={isEmpty(this.state.firstname, this.state.lastname, this.state.phonenumber) ? "none" : "auto"}
+                style={this.state.submitHover ? Styles.submitHover : Styles.submit}
+                    onTouchStart={() => { 
+                        this.setState({ submitHover: true }), 
+                        this.register(this.state.firstname, this.state.lastname, this.state.phonenumber)
+                    }}
+                    onTouchEnd={() => this.setState({ submitHover: false })}
+                >
+                    <Text style={Styles.label} >S'inscrire</Text>
                 </View>
             </View>
         )
