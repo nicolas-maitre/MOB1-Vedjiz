c53a8e724dfb5f3c04f430a245f320e4da94e8ed
Diogo <diogof-6@hotmail.com>
2020-08-10 19:26:34 +0200

    add begin of navigation

--- index.js
+++ index.js
@@ -3,9 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import DetailProduct from './views/DetailProduct';
-import HomePage from './views/HomePage';
-import List from './views/ListOfProducts';
+import App from './App';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => DetailProduct);
+AppRegistry.registerComponent(appName, () => App);
